fd2b332585538852fbe5f7cf69723aa398151188
Simon Da Vall <simon.davall@gmail.com>
2020-10-15 18:28:39 +0100

    pgs-41: Moved selection cards to separate partial and made TaskFilter list dynamic

---
+++ Pegasus/Entities/Enumerations/EnumHelper.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using Microsoft.AspNetCore.Authorization.Infrastructure;
+
+namespace Pegasus.Entities.Enumerations
+{
+    public static class EnumHelper<T> where T : struct, Enum
+    {
+        public static IList<T> GetValues(Enum value)
+        {
+            var enumValues = new List<T>();
+
+            foreach (FieldInfo fi in value.GetType().GetFields(BindingFlags.Static | BindingFlags.Public))
+            {
+                enumValues.Add((T)Enum.Parse(value.GetType(), fi.Name, false));
+            }
+            return enumValues;
+        }
+
+        public static T Parse(string value)
+        {
+            return (T)Enum.Parse(typeof(T), value, true);
+        }
+
+        public static IList<string> GetNames(Enum value)
+        {
+            if (value == null)
+                throw new ArgumentNullException(nameof(value), "Value cannot be null.");
+
+            return value.GetType().GetFields(BindingFlags.Static | BindingFlags.Public).Select(fi => fi.Name).ToList();
+        }
+
+        public static IList<string> GetDisplayValues(Enum value)
+        {
+            return GetNames(value).Select(name => GetDisplayValue(Parse(name))).ToList();
+        }
+
+        private static string LookupResource(Type resourceManagerProvider, string resourceKey)
+        {
+            foreach (PropertyInfo staticProperty in resourceManagerProvider.GetProperties(BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public))
+            {
+                if (staticProperty.PropertyType == typeof(System.Resources.ResourceManager))
+                {
+                    System.Resources.ResourceManager resourceManager = (System.Resources.ResourceManager)staticProperty.GetValue(null, null);
+                    if (resourceManager != null) 
+                        return resourceManager.GetString(resourceKey);
+                }
+            }
+
+            return resourceKey; // Fallback with the key name
+        }
+
+        public static string GetDisplayValue(T value)
+        {
+            var fieldInfo = value.GetType().GetField(value.ToString());
+
+            if (fieldInfo != null)
+            {
+                var descriptionAttributes = fieldInfo.GetCustomAttributes(
+                    typeof(DisplayAttribute), false) as DisplayAttribute[];
+
+                if (descriptionAttributes != null && descriptionAttributes[0].ResourceType != null)
+                    return LookupResource(descriptionAttributes[0].ResourceType, descriptionAttributes[0].Name);
+
+                if (descriptionAttributes == null) 
+                    return string.Empty;
+
+                return (descriptionAttributes.Length > 0) ? descriptionAttributes[0].Name : value.ToString();
+            }
+
+            return null;
+        }
+    }
+}
