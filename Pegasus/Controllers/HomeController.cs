4284a747e6f44e4c3a654437c8997531756e84fd
Simon Da Vall <simon.davall@gmail.com>
2020-10-31 10:17:29 +0000

    pgs-53: Added version to About page. Moved contact details to About page also.

--- Pegasus/Controllers/HomeController.cs
+++ Pegasus/Controllers/HomeController.cs
@@ -1,8 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using Pegasus.Domain.ProjectTask;
 using Pegasus.Library.Api;
-using Pegasus.Models;
 
 namespace Pegasus.Controllers
 {
@@ -11,17 +9,6 @@ namespace Pegasus.Controllers
         public HomeController(IConfiguration configuration, ITaskFilterService taskFilterService, 
             IProjectsEndpoint projectsEndpoint, ITasksEndpoint tasksEndpoint, ICommentsEndpoint commentsEndpoint) 
             : base(configuration, taskFilterService, projectsEndpoint, tasksEndpoint, commentsEndpoint)
-        {
-
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            var model = new BaseViewModel { ProjectId = 0 };
-
-            return View(model);
-        }
+        { }
     }
 }
