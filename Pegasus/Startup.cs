d91810cb0045fb547ae1e351118adb95908a875f
Simon Da Vall <simon.davall@gmail.com>
2020-11-10 12:05:04 +0000

    pgs-81: Added settings icon to login menu

--- Pegasus/Startup.cs
+++ Pegasus/Startup.cs
@@ -32,7 +32,8 @@ namespace Pegasus
 
             services.AddJwtAuthenticationWithProtectedCookie(Configuration);
 
-            services.AddControllersWithViews();
+            services.AddControllersWithViews()
+                .AddRazorRuntimeCompilation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
