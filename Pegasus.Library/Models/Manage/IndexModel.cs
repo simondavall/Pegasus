f5087fd22bba5ec92eb3ddc3a58b577a4c9696c7
Simon Da Vall <simon.davall@gmail.com>
2020-11-27 13:59:19 +0000

    pgs-99: Added manage layout for Manage section of site.

---
+++ Pegasus.Library/Models/Manage/IndexModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Pegasus.Library.Models.Manage
+{
+    public class IndexModel : ManageBaseModel
+    {
+        public string Username { get; set; }
+
+        [Phone]
+        [Display(Name = "Phone number")]
+        public string PhoneNumber { get; set; }
+    }
+}
