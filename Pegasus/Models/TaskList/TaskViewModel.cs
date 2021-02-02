169a421d40bd7207e42eb9a592d90049bdae96d2
Simon Da Vall <simon.davall@gmail.com>
2021-02-02 16:30:14 +0000

    pgs-25: Added link to parent task on task view

--- Pegasus/Models/TaskList/TaskViewModel.cs
+++ Pegasus/Models/TaskList/TaskViewModel.cs
@@ -8,34 +8,34 @@ namespace Pegasus.Models.TaskList
 {
     public class TaskViewModel : BaseViewModel
     {
-        public TaskModel ProjectTask { get; set; }
-        public IEnumerable<SelectListItem> TaskStatuses { get; set; }
-        public IEnumerable<SelectListItem> TaskTypes { get; set; }
-        public IEnumerable<SelectListItem> TaskPriorities { get; set; }
-        public ProjectModel Project { get; set; }
         public IEnumerable<TaskCommentModel> Comments { get; set; }
+        public int ExistingTaskStatus { get; set; }
         [Display(Name="Add Comment")]
         [DataType(DataType.MultilineText)]
         public string NewComment { get; set; }
-
+        public TaskModel ParentTask { get; set; }
+        public ProjectModel Project { get; set; }
+        public TaskModel ProjectTask { get; set; }
         public IEnumerable<TaskModel> SubTasks { get; set; }
-        
-        public int ExistingTaskStatus { get; set; }
+        public IEnumerable<SelectListItem> TaskPriorities { get; set; }
+        public IEnumerable<SelectListItem> TaskStatuses { get; set; }
+        public IEnumerable<SelectListItem> TaskTypes { get; set; }
 
         public static async Task<TaskViewModel> Create(TaskViewModelArgs args)
         {
             var model = new TaskViewModel
             {
-                ProjectId = args.ProjectTask.ProjectId,
-                TaskTypes = new SelectList(await args.TasksEndpoint.GetAllTaskTypes(), "Id", "Name", 1),
-                TaskStatuses = new SelectList(await args.TasksEndpoint.GetAllTaskStatuses(), "Id", "Name", 1),
-                TaskPriorities = new SelectList(await args.TasksEndpoint.GetAllTaskPriorities(), "Id", "Name", 1),
                 Comments = args.Comments ?? await args.CommentsEndpoint.GetComments(args.ProjectTask.Id),
-                ProjectTask = args.ProjectTask,
-                Project = args.Project ?? await args.ProjectsEndpoint.GetProject(args.ProjectTask.ProjectId),
                 ExistingTaskStatus = args.ExistingStatusId != 0 ? args.ExistingStatusId : args.ProjectTask.TaskStatusId,
                 NewComment = args.NewComment,
-                SubTasks = await args.TasksEndpoint.GetSubTasks(args.ProjectTask.Id)
+                ParentTask = await args.TasksEndpoint.GetTask(args.ProjectTask.ParentTaskId ?? 0),
+                Project = args.Project ?? await args.ProjectsEndpoint.GetProject(args.ProjectTask.ProjectId),
+                ProjectId = args.ProjectTask.ProjectId,
+                ProjectTask = args.ProjectTask,
+                SubTasks = await args.TasksEndpoint.GetSubTasks(args.ProjectTask.Id),
+                TaskPriorities = new SelectList(await args.TasksEndpoint.GetAllTaskPriorities(), "Id", "Name", 1),
+                TaskStatuses = new SelectList(await args.TasksEndpoint.GetAllTaskStatuses(), "Id", "Name", 1),
+                TaskTypes = new SelectList(await args.TasksEndpoint.GetAllTaskTypes(), "Id", "Name", 1)
             };
 
             return model;
