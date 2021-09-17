fd7a4a6d9654a35f498149a4f4b17ebf3fb3b6a8
Simon Da Vall <simon.davall@gmail.com>
2021-09-17 16:55:19 +0100

    pgs-113 - Updated SendGrid credentials. Forgot Password bug now fixed.

--- PegasusApi/Services/EmailSender.cs
+++ PegasusApi/Services/EmailSender.cs
@@ -23,15 +23,16 @@ namespace PegasusApi.Services
         public Task SendEmailAsync(string email, string subject, string message)
         {
             return Execute(Options.SendGridKey, subject, message, email);
-        }
+         }
 
         public Task Execute(string apiKey, string subject, string message, string email)
         {
             var client = new SendGridClient(apiKey);
+            var fromEmail = _configuration["Email:FromAddress"];
             var sender = _configuration["Email:Sender"];
-            var msg = new SendGridMessage()
+            var msg = new SendGridMessage
             {
-                From = new EmailAddress(sender, Options.SendGridUser),
+                From = new EmailAddress(fromEmail, sender),
                 Subject = subject,
                 PlainTextContent = message,
                 HtmlContent = message
