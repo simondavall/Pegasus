686ba72f1fe21ffae43fe602e8065c58fcbfa9a0
Simon Da Vall <simon.davall@gmail.com>
2021-10-18 15:59:36 +0100

    pgs-142: Refactored AccountController tests to smaller files (one test suite for each method)

---
+++ PegasusApi.Tests/Controllers/AccountController/ForgotPasswordTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using NUnit.Framework;
+using PegasusApi.Models.Account;
+
+namespace PegasusApi.Tests.Controllers.AccountController
+{
+    public class ForgotPasswordTests : AccountControllerTestsBase
+    {
+        
+        [Test]
+        public async Task ForgotPassword_UserIsNull_ReturnsModel()
+        {
+            MockUserManager.Setup(x => x.IsEmailConfirmedAsync(It.IsAny<IdentityUser>()));
+            MockUserManager.Setup(x => x.GeneratePasswordResetTokenAsync(It.IsAny<IdentityUser>()));
+            
+            var model = new ForgotPasswordModel { Email = EmailAddressNotFound };
+            var sut = CreateAccountController();
+            var result = await sut.ForgotPassword(model);
+
+            MockUserManager.Verify(x => x.IsEmailConfirmedAsync(It.IsAny<IdentityUser>()), Times.Never);
+            MockUserManager.Verify(x => x.GeneratePasswordResetTokenAsync(It.IsAny<IdentityUser>()), Times.Never);
+            result.Should().BeOfType<ForgotPasswordModel>();
+            result.Email.Should().Be(model.Email);
+        }
+
+        [Test]
+        public async Task ForgotPassword_EmailNotConfirmed_ReturnsModel()
+        {
+            MockUserManager.Setup(x => x.IsEmailConfirmedAsync(It.IsAny<IdentityUser>())).ReturnsAsync(false);
+            MockUserManager.Setup(x => x.GeneratePasswordResetTokenAsync(It.IsAny<IdentityUser>()));
+
+            var model = new ForgotPasswordModel { Email = EmailAddressGood };
+            var sut = CreateAccountController();
+            var result = await sut.ForgotPassword(model);
+
+            MockUserManager.Verify(x => x.IsEmailConfirmedAsync(It.IsAny<IdentityUser>()), Times.Once);
+            MockUserManager.Verify(x => x.GeneratePasswordResetTokenAsync(It.IsAny<IdentityUser>()), Times.Never);
+            result.Should().BeOfType<ForgotPasswordModel>();
+            result.Email.Should().Be(model.Email);
+        }
+
+        [Test]
+        public async Task ForgotPassword_TrySendEmail_CallsSendEmailAndReturnsModel()
+        {
+            MockUserManager.Setup(x => x.IsEmailConfirmedAsync(It.IsAny<IdentityUser>())).ReturnsAsync(true);
+            MockUserManager.Setup(x => x.GeneratePasswordResetTokenAsync(It.IsAny<IdentityUser>()))
+                .ReturnsAsync(string.Empty);
+            MockEmailSender.Setup(x => x.SendEmailAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()));
+
+            var model = new ForgotPasswordModel { Email = EmailAddressGood, BaseUrl = "https://example.com" };
+            var sut = CreateAccountController();
+            var result = await sut.ForgotPassword(model);
+
+            MockUserManager.Verify(x => x.GeneratePasswordResetTokenAsync(It.IsAny<IdentityUser>()), Times.Once);
+            MockEmailSender.Verify(x => x.SendEmailAsync(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+            result.Should().BeOfType<ForgotPasswordModel>();
+            result.Email.Should().Be(model.Email);
+        }
+
+    }
+}
---
+++ PegasusApi.Tests/Controllers/AccountController/ResetPasswordTests.cs
@@ -0,0 +1,98 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Microsoft.AspNetCore.Identity;
+using Moq;
+using NUnit.Framework;
+using PegasusApi.Library.Services.Resources;
+using PegasusApi.Models.Account;
+
+namespace PegasusApi.Tests.Controllers.AccountController
+{
+    public class ResetPasswordTests : AccountControllerTestsBase
+    {
+        
+        [Test]
+        public async Task ResetPassword_NoEmailAddress_ReturnsNoEmailError()
+        {
+            var sut = CreateAccountController();
+            var result = await sut.ResetPassword(new ResetPasswordModel());
+
+            result.Should().BeOfType<ResetPasswordModel>();
+            result.Errors.Should().NotBeNull();
+            result.Errors.Count().Should().Be(1);
+            result.Errors.First().Description.Should().Be(Resources.ControllerStrings.AccountController.EmailNotSupplied);
+        }
+
+        [Test]
+        public async Task ResetPassword_UserIsNull_ReturnsSucceededButDoesNotResetPassword()
+        {
+            MockUserManager.Setup(x => x.ResetPasswordAsync(It.IsAny<IdentityUser>(), It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Success);
+
+            var model = new ResetPasswordModel { UserId = UserId, Email = EmailAddressNotFound };
+            var sut = CreateAccountController();
+            var result = await sut.ResetPassword(model);
+
+            MockUserManager.Verify(x => x.FindByEmailAsync(It.IsAny<string>()), Times.Once);
+            MockUserManager.Verify(x => x.ResetPasswordAsync(It.IsAny<IdentityUser>(), It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+            result.Should().BeOfType<ResetPasswordModel>();
+            result.Succeeded.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task ResetPassword_UserNoSameIdAsModel_ReturnsSucceededButDoesNotResetPassword()
+        {
+            MockUserManager.Setup(x => x.ResetPasswordAsync(It.IsAny<IdentityUser>(), It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Success);
+
+            var model = new ResetPasswordModel { UserId = UnknownUserId, Email = EmailAddressGood };
+            var sut = CreateAccountController();
+            var result = await sut.ResetPassword(model);
+
+            MockUserManager.Verify(x => x.FindByEmailAsync(It.IsAny<string>()), Times.Once);
+            MockUserManager.Verify(x => x.ResetPasswordAsync(It.IsAny<IdentityUser>(), It.IsAny<string>(), It.IsAny<string>()), Times.Never);
+            result.Should().BeOfType<ResetPasswordModel>();
+            result.Succeeded.Should().BeTrue();
+        }
+
+        [Test]
+        public async Task ResetPassword_ResetFailed_ReturnsModelWithErrors()
+        {
+            var errors = new[] { new IdentityError { Code = string.Empty, Description = string.Empty } };
+            
+            MockUserManager.Setup(x =>
+                    x.ResetPasswordAsync(It.IsAny<IdentityUser>(), It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Failed(errors));
+
+            var model = new ResetPasswordModel { UserId = UserId, Email = EmailAddressGood };
+            var sut = CreateAccountController();
+            var result = await sut.ResetPassword(model);
+
+            MockUserManager.Verify(x => x.FindByEmailAsync(It.IsAny<string>()), Times.Once);
+            MockUserManager.Verify(x => x.ResetPasswordAsync(It.IsAny<IdentityUser>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+            result.Should().BeOfType<ResetPasswordModel>();
+            result.Succeeded.Should().BeFalse();
+            result.Errors.Should().NotBeNull();
+            result.Errors.Any().Should().BeTrue();
+        }
+
+        [Test]
+        public async Task ResetPassword_ResetSuccess_ReturnsModelWithNoErrors()
+        {
+            MockUserManager.Setup(x => x.ResetPasswordAsync(It.IsAny<IdentityUser>(), It.IsAny<string>(), It.IsAny<string>()))
+                .ReturnsAsync(IdentityResult.Success);
+
+            var model = new ResetPasswordModel { UserId = UserId, Email = EmailAddressGood };
+            var sut = CreateAccountController();
+            var result = await sut.ResetPassword(model);
+
+            MockUserManager.Verify(x => x.FindByEmailAsync(It.IsAny<string>()), Times.Once);
+            MockUserManager.Verify(x => x.ResetPasswordAsync(It.IsAny<IdentityUser>(), It.IsAny<string>(), It.IsAny<string>()), Times.Once);
+            result.Should().BeOfType<ResetPasswordModel>();
+            result.Succeeded.Should().BeTrue();
+            result.Errors.Should().BeNull();
+        }
+
+    }
+}
