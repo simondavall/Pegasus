12f9b36a89ec87b048b761df43a3b5f779df2547
Simon Da Vall <simon.davall@gmail.com>
2021-10-29 12:21:48 +0100

    pgs-159: Added script for Backup Single database

---
+++ PegasusSQLData/dbo/Scripts/pgs-159 - Create Backup Stored Procedure.sql
@@ -0,0 +1,37 @@
+﻿USE [master]
+GO
+
+/****** Object:  StoredProcedure [dbo].[sp_BackupSingleDatabase]    Script Date: 29/10/2021 00:31:20 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE PROCEDURE [dbo].[sp_BackupSingleDatabase]   
+            @databaseName sysname = null, 
+			@backupFileName nvarchar(128),
+            @backupLocation nvarchar(200)  
+AS  
+       SET NOCOUNT ON;  
+            DECLARE @DBs TABLE 
+            ( 
+                  ID int IDENTITY PRIMARY KEY, 
+                  DBNAME nvarchar(500) 
+            ) 
+
+            -- Declare variables 
+            DECLARE @BackupName nvarchar(100) 
+            DECLARE @BackupFile nvarchar(300) 
+            DECLARE @DBNAME nvarchar(300) 
+            DECLARE @sqlCommand NVARCHAR(1000)  
+
+		SET @DBNAME = '['+@databaseName+']' 
+		SET @BackupFile = @backupLocation + @backupFileName
+		SET @BackupName = REPLACE(REPLACE(@DBNAME,'[',''),']','') +' full backup'
+		SET @sqlCommand = 'BACKUP DATABASE ' +@DBNAME+  ' TO DISK = '''+@BackupFile+ ''' WITH INIT, NAME= ''' +@BackupName+''', NOSKIP, NOFORMAT' 
+ 
+		EXEC(@sqlCommand) 
+
+GO
