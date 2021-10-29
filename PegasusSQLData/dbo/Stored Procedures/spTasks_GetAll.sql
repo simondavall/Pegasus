4d3f6d8388b73e735a677f50494549e89663e09d
Simon Da Vall <simon.davall@gmail.com>
2021-10-29 20:42:20 +0100

    pgs-153: Removed archived projects' tasks from the All tasks list

--- PegasusSQLData/dbo/Stored Procedures/spTasks_GetAll.sql
+++ PegasusSQLData/dbo/Stored Procedures/spTasks_GetAll.sql
@@ -1,9 +1,9 @@
 ﻿CREATE PROCEDURE [dbo].[spTasks_GetAll]
 
 AS
-	SELECT *
-	FROM ProjectTasks
-	WHERE ParentTaskId is null
+	SELECT pt.*
+	FROM ProjectTasks pt JOIN Projects p ON pt.ProjectId = p.Id
+	WHERE ParentTaskId is null AND p.IsDeleted = 0
 	ORDER BY Modified DESC
 
 RETURN 0
