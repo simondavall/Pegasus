09ce7bfc1e622192fd3648751d954e1a64f4d47f
Simon Da Vall <simon.davall@gmail.com>
2021-12-03 10:00:39 +0000

    pgs-171: Refactored TaskModelExtensions and fixed broken unit tests.

--- Pegasus/Extensions/TaskModelExtensions.cs
+++ Pegasus/Extensions/TaskModelExtensions.cs
@@ -9,73 +9,6 @@ namespace Pegasus.Extensions
 {
     public static class TaskModelExtensions
     {
-        public static bool IsClosed(this TaskModel model)
-        {
-            return model.TaskStatusId == (int) TaskStatusEnum.Completed ||
-                   model.TaskStatusId == (int) TaskStatusEnum.Obsolete;
-        }
-
-        public static string PriorityIconClass(this TaskModel model)
-        {
-            if (model.TaskPriorityId <= (int) TaskPriorityEnum.Normal)
-            {
-                return string.Empty;
-            }
-            
-            return model.IsClosed() ? "fa fa-bolt priority-icon-closed" 
-                : $"fa fa-bolt priority-icon-{model.TaskPriorityId}";
-        }
-
-        public static bool HasParentTask(this TaskModel model)
-        {
-            return model.ParentTaskId != null;
-        }
-        
-        public static ITaskProfile TaskProfile(this TaskModel model)
-        {
-            return TaskProfileResolver.GetTaskProfile((TaskStatusEnum)model.TaskStatusId);
-        }
-
-        private static IEnumerable<TaskModel> IsNotClosed(this IEnumerable<TaskModel> projectTasks)
-        {
-            return projectTasks.IsNotCompleted().IsNotObsolete();
-        }
-
-        private static IEnumerable<TaskModel> IsNotCompleted(this IEnumerable<TaskModel> projectTasks)
-        {
-            return projectTasks.Where(pt => pt.TaskStatusId != (int)TaskStatusEnum.Completed);
-        }
-
-        private static IEnumerable<TaskModel> IsHighPriority(this IEnumerable<TaskModel> projectTasks)
-        {
-            return projectTasks.Where(pt => pt.TaskPriorityId >= (int)TaskPriorityEnum.High);
-        }
-
-        private static IEnumerable<TaskModel> IsInBacklog(this IEnumerable<TaskModel> projectTasks)
-        {
-            return projectTasks.Where(pt => pt.TaskStatusId == (int)TaskStatusEnum.Backlog);
-        }
-
-        private static IEnumerable<TaskModel> IsNotObsolete(this IEnumerable<TaskModel> projectTasks)
-        {
-            return projectTasks.Where(pt => pt.TaskStatusId != (int)TaskStatusEnum.Obsolete);
-        }
-
-        private static IEnumerable<TaskModel> IsNotInBacklog(this IEnumerable<TaskModel> projectTasks)
-        {
-            return projectTasks.Where(pt => pt.TaskStatusId != (int)TaskStatusEnum.Backlog);
-        }
-
-        private static IEnumerable<TaskModel> IsObsolete(this IEnumerable<TaskModel> projectTasks)
-        {
-            return projectTasks.Where(pt => pt.TaskStatusId == (int)TaskStatusEnum.Obsolete);
-        }
-
-        public static IEnumerable<TaskModel> Sorted(this IEnumerable<TaskModel> projectTasks, ISorter sorter)
-        {
-            return sorter.Sort(projectTasks);
-        }
-
         public static IEnumerable<TaskModel> Filtered(this IEnumerable<TaskModel> projectTasks, int taskFilterId)
         {
             switch ((TaskFilters)taskFilterId)
@@ -91,6 +24,73 @@ namespace Pegasus.Extensions
                 default:
                     return projectTasks.IsNotObsolete();
             }
+        }
+
+        public static bool HasParentTask(this TaskModel model)
+        {
+            return model.ParentTaskId != null;
+        }
+
+        public static bool IsClosed(this TaskModel model)
+        {
+            return model.TaskStatusId == (int)TaskStatusEnum.Completed ||
+                   model.TaskStatusId == (int)TaskStatusEnum.Obsolete;
+        }
+
+        private static IEnumerable<TaskModel> IsHighPriority(this IEnumerable<TaskModel> projectTasks)
+        {
+            return projectTasks.Where(task => task.TaskPriorityId >= (int)TaskPriorityEnum.High);
+        }
+
+        private static IEnumerable<TaskModel> IsInBacklog(this IEnumerable<TaskModel> projectTasks)
+        {
+            return projectTasks.Where(task => task.TaskStatusId == (int)TaskStatusEnum.Backlog);
+        }
+
+        private static IEnumerable<TaskModel> IsNotClosed(this IEnumerable<TaskModel> projectTasks)
+        {
+            return projectTasks.IsNotCompleted().IsNotObsolete();
+        }
+
+        private static IEnumerable<TaskModel> IsNotCompleted(this IEnumerable<TaskModel> projectTasks)
+        {
+            return projectTasks.Where(task => task.TaskStatusId != (int)TaskStatusEnum.Completed);
+        }
+
+        private static IEnumerable<TaskModel> IsNotInBacklog(this IEnumerable<TaskModel> projectTasks)
+        {
+            return projectTasks.Where(task => task.TaskStatusId != (int)TaskStatusEnum.Backlog);
+        }
+
+        private static IEnumerable<TaskModel> IsNotObsolete(this IEnumerable<TaskModel> projectTasks)
+        {
+            return projectTasks.Where(task => task.TaskStatusId != (int)TaskStatusEnum.Obsolete);
+        }
+
+        private static IEnumerable<TaskModel> IsObsolete(this IEnumerable<TaskModel> projectTasks)
+        {
+            return projectTasks.Where(task => task.TaskStatusId == (int)TaskStatusEnum.Obsolete);
+        }
+
+        public static string PriorityIconClass(this TaskModel model)
+        {
+            if (model.TaskPriorityId <= (int)TaskPriorityEnum.Normal)
+            {
+                return string.Empty;
+            }
+
+            return model.IsClosed() ? "fa fa-bolt priority-icon-closed"
+                : $"fa fa-bolt priority-icon-{model.TaskPriorityId}";
+        }
+
+        public static IEnumerable<TaskModel> Sorted(this IEnumerable<TaskModel> projectTasks, ISorter sorter)
+        {
+            return sorter.Sort(projectTasks);
+        }
+
+        public static ITaskProfile TaskProfile(this TaskModel model)
+        {
+            return TaskProfileResolver.GetTaskProfile((TaskStatusEnum)model.TaskStatusId);
         }
     }
 }
